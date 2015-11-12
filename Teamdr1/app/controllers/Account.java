7db359c6610c167dbd6be32fd0b2edb9f6efa72a
bluemelodia <hsumelanie@yahoo.com>
2015-11-11 22:57:23 -0500

    Successfully passed parameters into the sign up page.

--- Teamdr1/app/controllers/Account.java
+++ Teamdr1/app/controllers/Account.java
@@ -1,18 +1,22 @@
 package controllers;
+import models.UserAccount;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.data.Form;
 import views.html.*;
 
 /**
  * Created by bluemelodia on 11/11/15.
  */
 public class Account extends Controller {
-    public Result list() {
+    // Enables passing of params into the form
+    private static final Form<Account> AccountForm = Form.form(Account.class);
+    public Result createUser() {
         return TODO;
     }
 
     public Result newAccount() {
-        return ok(account.render());
+        return ok(account.render(AccountForm));
     }
 
     public Result signIn() {
