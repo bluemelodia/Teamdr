bda4f540ff1738a69df5e8e60e1e7ec304c45322
bluemelodia <hsumelanie@yahoo.com>
2015-11-12 17:29:30 -0500

    Added fake path to stop crashing.

--- Teamdr1/conf/evolutions/default/1.sql
+++ Teamdr1/conf/evolutions/default/1.sql
@@ -3,11 +3,19 @@
 
 # --- !Ups
 
+create table team_lookup (
+  username                  varchar(255) not null,
+  password                  varchar(255),
+  constraint pk_team_lookup primary key (username))
+;
+
 create table user_account (
   username                  varchar(255) not null,
   password                  varchar(255),
   constraint pk_user_account primary key (username))
 ;
+
+create sequence team_lookup_seq;
 
 create sequence user_account_seq;
 
@@ -18,9 +26,13 @@ create sequence user_account_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists team_lookup;
+
 drop table if exists user_account;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists team_lookup_seq;
 
 drop sequence if exists user_account_seq;
 
