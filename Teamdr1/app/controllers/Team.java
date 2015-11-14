056824b6e744c05ebb6d09df82aaa930a6ec15b3
bluemelodia <hsumelanie@yahoo.com>
2015-11-14 14:46:38 -0500

    Did error checking for create team function.

--- Teamdr1/app/controllers/Team.java
+++ Teamdr1/app/controllers/Team.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import models.ClassRecord;
+import models.TeamRecord;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -37,17 +38,49 @@ public class Team extends Controller {
     }
 
     public Result showCreateTeamPage() {
-        JsonNode json = toJson("COMS4111");
-        return ok(createteam.render(json));
+        JsonNode className = toJson("COMS4111");
+        JsonNode error = toJson("");
+        return ok(createteam.render(className, error, error));
     }
 
+    // Create a team, validate params before adding the new team to the database
     public Result createTeam() {
         final Map<String, String[]> values = request().body().asFormUrlEncoded();
         String teamName = values.get("teamName")[0];
         String tid = values.get("teamID")[0];
-        System.out.println(teamName + " " + tid);
+        JsonNode error;
+        JsonNode error2;
+        JsonNode className = toJson("COMS4111");
+
+        if (teamName.length() < 1 && tid.length() < 1) {
+            error = toJson("TeamName is required.");
+            error2 = toJson("TeamID is required.");
+            return badRequest(createteam.render(className, error, error2));
+        } else if (teamName.length() < 1) {
+            error = toJson("TeamName is required.");
+            error2 = toJson("");
+            return badRequest(createteam.render(className, error, error2));
+        } else if (tid.length() < 1) {
+            error = toJson("");
+            error2 = toJson("TeamID is required.");
+            return badRequest(createteam.render(className, error, error2));
+        }
+
+        // Check to see if the team already exists
+        if (TeamRecord.exists(tid)) {
+            error = toJson("Team ID already exists!");
+            error2 = toJson("");
+            return badRequest(createteam.render(className, error, error2));
+        }
+
+        // Create a new team and add to the database
+        // Fill in user, class, etc.
+        // User cannot have more than one team for this class
+
         JsonNode json = toJson("COMS4111");
-        return ok(createteam.render(json));
+        error = toJson("");
+        error2 = toJson("");
+        return ok(createteam.render(json, error, error2));
     }
 
 }
