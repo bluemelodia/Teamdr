7f56f86260c1e931e2e9637a196cf9906adff5f8
bluemelodia <hsumelanie@yahoo.com>
2015-11-15 21:01:03 -0500

    Showed number of notifications on dashboard.

--- Teamdr1/app/controllers/Profile.java
+++ Teamdr1/app/controllers/Profile.java
@@ -32,7 +32,7 @@ public class Profile extends Controller {
         // If the user has notifications, show them
         String notifs = "Hi";
         if (Notifications.hasNotifs(getUser.username)) {
-            notifs = "You have notifications: ";
+            notifs = "You have " + Notifications.countNotifs(getUser.username) + " notifications: https://www.google.com";
         }
 
         JsonNode user_json = toJson(getUser);
@@ -71,7 +71,7 @@ public class Profile extends Controller {
         // If the user has notifications, show them
         String notifs = "Hi";
         if (Notifications.hasNotifs(getUser.username)) {
-            notifs = "You have notifications: ";
+            notifs = "You have " + Notifications.countNotifs(getUser.username) + " notifications: https://www.google.com";
         }
 
 		JsonNode user_json = toJson(getUser);
