657abdc4aff8bce631913719860c627cafa52caa
Amel Abid <abidamal20@gmail.com>
2015-12-10 23:06:44 -0500

    Wrote some tests to the TeamRecordTest.java

--- Teamdr1/test/TeamRecordTest.java
+++ Teamdr1/test/TeamRecordTest.java
@@ -1,24 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.avaje.ebean.Ebean;
+import models.UserAccount;
+import org.junit.Test;
+import play.test.WithApplication;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-import play.twirl.api.Content;
-
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
-
+import static models.TeamRecord.createTeamRecord;
+import static models.TeamRecord.exists;
+import static org.junit.Assert.assertEquals;
 
 /**
 *
@@ -26,7 +13,7 @@ import static org.junit.Assert.*;
 * If you are interested in mocking a whole application, see the wiki for more details.
 *
 */
-public class TeamRecordTest {
+public class TeamRecordTest extends WithApplication {
 
     @Test
     public void simplerCheck() {
@@ -34,5 +21,52 @@ public class TeamRecordTest {
         assertEquals(1, a);
     }
 
+    @Test
+    public void testCreateTeamRecord() {
+        String tid = "testTeam15";
+        String teamName = "testTeamName";
+        String thisClass = "COMS4118";
+        UserAccount user = new UserAccount();
+        user.username = "testUser15";
+        Ebean.save(user);
+
+        models.TeamRecord team;
+        team = createTeamRecord(tid, user, teamName, thisClass);
+        Ebean.save(team);
+        assert(exists(tid));
+        Ebean.delete(user);
+        Ebean.delete(team);
+    }
+
+   @Test
+    public void testGetTeam() {
+        String tid = "testTeam20";
+        String teamName = "testTeamName";
+        String thisClass = "COMS4118";
+        UserAccount user = new UserAccount();
+        user.username = "testUser20";
+        Ebean.save(user);
+        models.TeamRecord team =createTeamRecord(tid, user, teamName, thisClass);
+        Ebean.save(team);
+        assertEquals(models.TeamRecord.getTeam(tid).tid, tid);
+        Ebean.delete(user);
+        Ebean.delete(team);
+    }
+
+    @Test
+    public void testGetTeamForClass() {
+
+        UserAccount user = new UserAccount();
+        user.username = "testUser10";
+        Ebean.save(user);
+        models.TeamRecord team = createTeamRecord("teamID10", user, "teamName", "COMS4118");
+        Ebean.save(team);
+        assertEquals(models.TeamRecord.getTeamForClass(user.username, "COMS4118").tid, "teamID10");
+        Ebean.delete(user);
+        Ebean.delete(team);
+    }
+
+  //  @Test
+  //  public void
 
 }
